2512d698d2a1b9727d402d3b1bcde5152949e752
PauloGN <paulorsantos.gnt@outlook.com>
2022-04-25 13:17:16 -0700

    Store screen Animation

--- CookieClicker/Assets/Scripts/StoreScreen.cs
+++ CookieClicker/Assets/Scripts/StoreScreen.cs
@@ -8,8 +8,15 @@ public class StoreScreen : MonoBehaviour
 
     [SerializeField] private ScreenManager _screenManager = null;
     [SerializeField] private GameManager _GameManager = null;
+    [SerializeField] private Animator _animator = null;
 
+    //Get back to the main screen
     public void OnClose()
+    {
+        _animator.SetTrigger("Close");
+    }
+
+    public void OnCloseFinishAnimation()
     {
 
         _screenManager.CloseScreen();
