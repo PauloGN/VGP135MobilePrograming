4504410a88e0d4b49d4c7834ffe9133a7ecfe947
Paulo Roberto dos Santos <paulorsantos.gnt@outlook.com>
2022-05-06 21:53:12 -0700

    Particle System

--- KetchAppStyleGame/Assets/Scripts/PlayerHealth.cs
+++ KetchAppStyleGame/Assets/Scripts/PlayerHealth.cs
@@ -7,10 +7,12 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private ScoreTrack scoreTrackREF;
     [SerializeField] private int bonus;
     [SerializeField] private GameOverPanel gameOverPanel = null;
+    [SerializeField] private GameObject explosion;
     public void crash()
     {
         gameObject.SetActive(false);
         gameOverPanel.OnGameOver();
+        Instantiate(explosion, transform.position, transform.rotation);
     }
 
     public void SetScore()
