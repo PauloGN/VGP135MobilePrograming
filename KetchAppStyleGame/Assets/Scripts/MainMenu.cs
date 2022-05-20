44877546bf88960644a80bec78e26ccd01bf885f
PauloGN <paulorsantos.gnt@outlook.com>
2022-05-20 16:20:10 -0700

    Revert "skyBox"
    This reverts commit 4e6f9169490302069c574193dcb190704b0c2683.

--- KetchAppStyleGame/Assets/Scripts/MainMenu.cs
+++ KetchAppStyleGame/Assets/Scripts/MainMenu.cs
@@ -8,25 +8,17 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] private TMP_Text scoreTxt;
 
-  
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
 
     private void Start()
     {
-        
         if (SaveGameData.Instance.LoadGameScore()._scores.Length >= 1)
         {
             scoreTxt.text = "Leader Score: " + SaveGameData.Instance.LoadGameScore()._scores[0].ToString();
         }
-       // AdsManager.instance.ShowBanner();
     }
-
-
-
-    public void StartGame()
-    {        
-       AdsManager.instance.HideBanner(); 
-       SceneManager.LoadScene(1);
-    }
-
 
 }
